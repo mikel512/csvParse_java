b4e088e29c3e2db69bb7ae3e184b02f7ee95b78e
Mikel <mikelmayorga08@gmail.com>
2020-11-07 02:56:12 -0800

    modified: ORM functionality works with local MySQL database

---
+++ src/DAL/SqlAccess.java
@@ -0,0 +1,40 @@
+package DAL;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.stream.Stream;
+
+public class SqlAccess<T> {
+    private SessionFactory sessionFactory;
+    private Stream<T> objectStream;
+
+    private SqlAccess (Stream<T> objectStream,SessionFactory sessionFactory) {
+        this.objectStream = objectStream;
+        this.sessionFactory = sessionFactory;
+    }
+
+    /**
+     * Creates a SqlAccess instance
+     * @param stream a {@link Stream} containing {@code n} objects of type {@code T}.
+     * @param <T> the type corresponding to the stream objects
+     * @return a new SqlAccess instance
+     */
+    public static <T> SqlAccess<T> newInstance (Stream<T> stream, SessionFactory sessionFactory) {
+        return new SqlAccess<>(stream, sessionFactory);
+    }
+
+
+    /**
+     * Inserts each object in a {@link Stream} to its corresponding table using Hibernate ORM
+     */
+    public void insertObjects() {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        objectStream.forEach(o -> session.save(o));
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+}
--- src/com/parse/Main.java
+++ src/com/parse/Main.java
@@ -1,17 +1,34 @@
 package com.parse;
 
-import Logic.CsvParse;
-import Logic.SbaParse;
-import Models.SacramentoCrimeJanuary;
+import DAL.SqlAccess;
+import models.SacramentoCrimeJanuary;
+import models.SacramentoRealEstateTransactions;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import utilities.CsvParseBean;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.module.Configuration;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).addAnnotatedClass(SacramentoCrimeJanuary.class).buildMetadata().buildSessionFactory();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("src/data/SacramentocrimeJanuary2006.csv"));
+            CsvParseBean<SacramentoCrimeJanuary> instance = CsvParseBean.newInstance(br, SacramentoCrimeJanuary.class);
+            var transactions = instance.rows();
+            SqlAccess<SacramentoCrimeJanuary> access = SqlAccess.newInstance(transactions, sessionFactory);
+            access.insertObjects();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
