2ba9e5a86339111cd4f0881bb998a6cde55729d5
Mikel <mikelmayorga08@gmail.com>
2020-11-08 23:58:26 -0800

    added: CreateClassFromCsv tool- creates a JavaBean from a specified csv file

---
+++ src/com/parse/utilities/CreateClassFromCsv.java
@@ -0,0 +1,99 @@
+package com.parse.utilities;
+
+import java.io.*;
+
+/**
+ * This class creates a .java file which contains a class adhering to the JavaBean standard from a CSV file
+ * The name of the class within this new file will match the name of the .csv file
+ * The properties of this new class will be extracted from the first line of the CSV
+ * For purposes of this project it will also include Hibernate annotations for easy SQL access using ORM as an option
+ */
+public class CreateClassFromCsv {
+    private boolean annotations = false;
+
+    public CreateClassFromCsv withAnnotations() {
+        this.annotations = true;
+        return this;
+    }
+
+    public void createFile(String csvName, String firstLn) {
+        String name = csvName.substring(0, csvName.lastIndexOf('.'));
+        File file = createJavaFile(name);
+        writeClass(file, firstLn.split(","), name);
+    }
+
+    private void writeClass(File file, String[] firstLn, String csvName) {
+        try{
+            FileWriter writer = new FileWriter(file);
+            // first write default boilerplate
+            writer.write("package com.parse.models \n\n");
+            if(annotations) {
+                writer.write("import javax.persistence.* \n\n" );
+                writer.write("@Entity \n");
+                writer.write("@Table (name =" + "\""+ csvName.toLowerCase() + "\") \n" );
+            }
+            writer.write("public class " + csvName + " implements java.io.Serializable { \n");
+
+            // write private props
+            writer.write("\tprivate Long id; \n");
+            for(String prop : firstLn) {
+                writer.write(" \tprivate String " + prop + ";\n");
+            }
+
+            // write default constructor
+            writer.write("\n\tpublic " + csvName + "() {}\n\n");
+
+            // generate getters and setters
+            writer.write(createGetterSetter("id" , true));
+            for(String prop : firstLn) {
+                writer.write(createGetterSetter(prop, false));
+            }
+
+            writer.write("\n }\n");
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private String createGetterSetter(String propName, boolean isId) {
+        String capitalize = propName.substring(0, 1).toUpperCase() + propName.substring(1);
+        String result;
+        // create setter
+        if(isId) {
+            result = "\tpublic void set" + capitalize + "(Long " + propName + ") {\n";
+            result += "\t\tthis." + propName + " = " + propName + ";\n\t}\n\n";
+        } else {
+            result = "\tpublic void set" + capitalize + "(String " + propName + ") {\n";
+            result += "\t\tthis." + propName + " = " + propName + ";\n\t}\n\n";
+        }
+        // create getter
+        if(annotations){
+            result += "\t@Column (name = \"" + propName + "\")\n";
+        }
+        if(isId){
+            result += "\tpublic Long get" + capitalize + "() {\n";
+            result += "\t\t return " + propName + ";\n\t}\n\n";
+        } else {
+            result += "\tpublic String get" + capitalize + "() {\n";
+            result += "\t\t return " + propName + ";\n\t}\n\n";
+        }
+        return result;
+    }
+
+    private File createJavaFile(String csvName) {
+        try{
+            File file = new File("src/com/parse/models/" + csvName + ".java");
+            if(file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            }
+            else {
+                System.out.println("File already exists.");
+            }
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
